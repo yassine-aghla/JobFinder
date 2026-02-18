e6f6797b46ad0fe9b91021868fec5d24bcc3449d
yassine <yassineaghla23@gmail.com>
2026-02-18 02:11:38 +0000

    modifier app config pour etre adaptable avec ngrx

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,22 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
+import { provideStore } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
 import { routes } from './app.routes';
-
+import { favoritesReducer } from './store/favorites.reducer';
+import { FavoritesEffects } from './store/favorites.effects';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideHttpClient()
+    provideHttpClient(),
+    provideStore({ favorites: favoritesReducer }),
+    provideEffects(FavoritesEffects),
+    provideStoreDevtools({
+      maxAge: 25,
+      logOnly: false
+    })
   ]
 };
