fd3033ac8a7530ab5a0bb7cb7d21c9b50fe466a2
yassine <yassineaghla23@gmail.com>
2026-02-18 00:16:25 +0000

    l'implementation de model user pour la gstion des users

---
+++ src/app/models/user.ts
@@ -0,0 +1,14 @@
+export interface User {
+  id?: number;
+  firstName: string;
+  lastName: string;
+  email: string;
+  password?: string;
+}
+
+export interface UserSession {
+  id: number;
+  firstName: string;
+  lastName: string;
+  email: string;
+}
