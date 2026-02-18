322395f503f4cf996c17168f5eb57bad019ea8c3
yassine <yassineaghla23@gmail.com>
2026-02-18 00:49:44 +0000

    ipmlement un header personaliser pour tous les pages

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [CommonModule, RouterModule],
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent {
+  constructor(
+    public authService: AuthService,
+    private router: Router
+  ) {}
+
+  logout(): void {
+    this.authService.logout();
+    this.router.navigate(['/jobs']);
+  }
+}
