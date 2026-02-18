4bbcbff0af94dba5cd41f278350b157b74d11f7e
yassine <yassineaghla23@gmail.com>
2026-02-18 00:30:01 +0000

    modifier routes pour la page login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 import { authGuard } from './guards/auth.guard';
+import {LoginComponent} from './login/login.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'jobs', pathMatch: 'full' },
@@ -9,6 +10,10 @@ export const routes: Routes = [
       {
         path: 'register',
         loadComponent: () => import('./register/register.component').then(m => m.RegisterComponent)
+      },
+      {
+        path: 'login',
+        loadComponent: () => import('./login/login.component').then(m => m.LoginComponent)
       }
     ]
   },
