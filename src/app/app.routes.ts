e0aec5344029c66075afe18c01c920f85ccab294
yassine <yassineaghla23@gmail.com>
2026-02-18 00:19:04 +0000

    creation des fichier des configuration avec les commandes de terminal

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { authGuard } from './guards/auth.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'jobs', pathMatch: 'full' },
+  {
+    path: 'auth',
+    children: [
+      {
+        path: 'register',
+        loadComponent: () => import('./register/register.component').then(m => m.RegisterComponent)
+      }
+    ]
+  },
+  { path: '**', redirectTo: 'jobs' }
+];
